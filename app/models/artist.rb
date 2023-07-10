e3da69596e7ffdb8bf993a46ebfa0e96a481b457
Cristhian Vera <cristianmario092190@gmail.com>
2023-07-09 19:52:55 -0500

    Task 4: Create the validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,21 @@
 class Artist < ApplicationRecord
+  validates :name, presence: true
+  validate :born_in_the_future
+  validate :before_birth_date
+
+  # nacido en un futuro
+  def born_in_the_future
+    return nil if birth_date.nil?
+    return errors.add(:birth_date, "can't be in the future") if birth_date > Time.zone.today
+  end
+
+  # antes de la fecha de nacimiento
+  def before_birth_date
+    return nil if death_date.nil?
+    return errors.add(:death_date, "you do not have a birth_date") if birth_date.nil?
+    return unless death_date < birth_date
+
+    errors.add(:death_date,
+               "death_date can't be after the birth_date")
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  validates :username , presence: true , uniqueness: true
+  validates :email , uniqueness:true , format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password , presence: true , length: { minimum: 6 } , format: { with: /\A(?=.*\d).+\z/ }
 end
