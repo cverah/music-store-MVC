595808c2bba5e3f24f8aa89dab0d45d1605ce178
Cristhian Vera <cristianmario092190@gmail.com>
2023-07-10 07:49:48 -0500

    Task 6: Create seed data-create 10 artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "faker"
+
+# Create 10 artists.
+puts "Creating 10 artists"
+artist = Artist.all
+# p artist.first.nationality
+10.times do
+  birth_date =  Faker::Date.birthday(min_age: 18, max_age: 90)
+  artist = Artist.create(
+    name: Faker::Music.band,
+    nationality: Faker::Address.country,
+    birth_date: birth_date,
+    # death date mas 18 years
+    death_date: Faker::Date.between(from: birth_date +18 *365, to: Date.today),
+    biography: Faker::Music.genre
+  )
+  unless artist.persisted?
+    puts artist.errors.full_messages.join(",")
+    p artist
+  end
+end
+
+puts "Finished! Creating 10 artists"
+
