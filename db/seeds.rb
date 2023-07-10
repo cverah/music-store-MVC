d861a4a34662a85ab8d67cba8111b8773b4dbbc1
Cristhian Vera <cristianmario092190@gmail.com>
2023-07-10 08:01:49 -0500

    Task 6: Create between 2 to 6 albums for each artist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,12 @@
 require "faker"
 
+# destroy all data
+
+Album.destroy_all
+Artist.destroy_all
+
 # Create 10 artists.
 puts "Creating 10 artists"
-artist = Artist.all
 # p artist.first.nationality
 10.times do
   birth_date =  Faker::Date.birthday(min_age: 18, max_age: 90)
@@ -22,3 +26,22 @@ end
 
 puts "Finished! Creating 10 artists"
 
+# Create between 2 to 6 albums for each artist.
+puts "Creating between 2 to 6 albums for each artist"
+artists = Artist.all
+# p artists.count
+artists.each do |artist|
+  rand(1..6).times do
+    album = Album.create(
+      name: Faker::Music.album,
+      price: Faker::Commerce.price,
+      duration: nil, 
+      artist_id: artist.id
+    )
+    unless album.persisted?
+      puts album.errors.full_messages.join(",")
+      p album
+    end
+  end
+end
+puts "Finish creating between 2 to 6 albums for each artist"
